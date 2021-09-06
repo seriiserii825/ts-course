8007dfc859fd0c2624f380481db07138c0f557e1
serii <seriiburduja@gmail.com>
2021-09-06 12:46:05 +0300

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = "https://jsonplaceholder.typicode.com/todos/";
+axios_1["default"].get(url).then(function (res) { return console.log(res.data, "res.data"); });
---
+++ index.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/";
+
+axios.get(url).then((res) => {
+  console.log(res.data);
+});
