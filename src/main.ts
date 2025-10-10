3fbc70e01f4bb1e5a15eed1b7eb030aeddd036e4
serii <seriiburduja@gmail.com>
2025-10-10 08:19:16 +0300

    feat: nominal type

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,12 @@
-import { z } from "zod";
+type UserId = string & {__brand: "user_id"};
+type PostId = string & {__brand: "post_id"};
 
-// Валидная модель того, что мы хотим получить
-export const PersonSchema = z.object({
-  name: z.string(),
-  age: z.number(),
-});
-
-// Тип берём из схемы — всегда синхронен с валидацией
-export type Person = z.infer<typeof PersonSchema>;
-
-
-async function fetchJSON(url: string): Promise<unknown> {
-  const res = await fetch(url);
-
-  // базовые проверки сети/статуса
-  if (!res.ok) {
-    throw new Error(`HTTP ${res.status} for ${url}`);
-  }
-  // необязательно, но полезно: проверим тип контента
-  const ct = res.headers.get("content-type") || "";
-  if (!ct.includes("application/json")) {
-    throw new Error(`Expected JSON, got ${ct}`);
-  }
-  return res.json(); // <- unknown, не any
+function getUserId(id: UserId): UserId {
+  return id;
 }
 
-/**
- * Безопасный fetch с проверкой схемой.
- * Если сервер "сломал контракт", сразу бросит ошибку.
- */
-export async function fetchWithSchema<T>(
-  url: string,
-  schema: z.ZodType<T>
-): Promise<T> {
-  const data = await fetchJSON(url);
-  // Бросит ZodError, если форма данных не совпадает
-  return schema.parse(data);
-}
+const user_id = 'user_id' as UserId;
+const post_id = 'post_id' as PostId;
 
-
-const PeopleSchema = z.array(PersonSchema);
-
-export async function getPeople(): Promise<Person[]> {
-  return fetchWithSchema<Person[]>("/api/people", PeopleSchema);
-}
-
-(async () => {
-  const ppl = await getPeople(); // <- гарантированно Person[]
-  // здесь уже полноценный тип, автодополнение и проверки
-  console.log(ppl[0].name.toUpperCase());
-})();
+getUserId(user_id)
+getUserId(post_id)
