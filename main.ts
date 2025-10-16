ca46a8c59cf752aaaa64e306ab766590d756111a
serii burduja <seriiburduja@gmail.com>
2025-10-16 10:10:55 +0300

    feat: 29

---
+++ main.ts
@@ -0,0 +1,62 @@
+// wrong way
+
+interface State {
+  pageText: string;
+  isLoading: boolean;
+  error?: string;
+}
+
+function renderPage(state: State, currentPage: string): string {
+  if (state.error) {
+    return `Error! Unable to load ${currentPage}: ${state.error}`;
+  } else if (state.isLoading) {
+    return `Loading ${currentPage}...`;
+  }
+  return `<h1>${currentPage}</h1>\n${state.pageText}`;
+}
+async function changePage(state: State, newPage: string) {
+  state.isLoading = true;
+  try {
+    const response = await fetch(getUrlForPage(newPage));
+    if (!response.ok) {
+      throw new Error(`Unable to load ${newPage}: ${response.statusText}`);
+    }
+    const text = await response.text();
+    state.isLoading = false;
+    state.pageText = text;
+  } catch (e) {
+    state.error = "" + e;
+  }
+}
+
+// right way
+interface RequestPending {
+  state: "pending";
+}
+interface RequestError {
+  state: "error";
+  error: string;
+}
+interface RequestSuccess {
+  state: "ok";
+  pageText: string;
+}
+type RequestState = RequestPending | RequestError | RequestSuccess;
+
+interface State {
+  currentPage: string;
+  requests: { [page: string]: RequestState };
+}
+
+function renderCustomPage(state: State) {
+  const { currentPage } = state;
+  const requestState = state.requests[currentPage];
+  switch (requestState.state) {
+    case "pending":
+      return `Loading ${currentPage}...`;
+    case "error":
+      return `Error! Unable to load ${currentPage}: ${requestState.error}`;
+    case "ok":
+      return `<h1>${currentPage}</h1>\n${requestState.pageText}`;
+  }
+}
