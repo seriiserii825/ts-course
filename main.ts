15c7212131bd7e725b21f7b6df9a7de37af810e7
serii <seriiburduja@gmail.com>
2025-10-17 09:03:10 +0300

    feat: main ts

--- main.ts
+++ main.ts
@@ -1,62 +1,45 @@
 // wrong way
 
-interface State {
-  pageText: string;
-  isLoading: boolean;
-  error?: string;
+type HexColor = `#${string}`;
+const color: HexColor = "#FF5733"; // Correct
+
+type RGBColor = `rgb(${number}, ${number}, ${number})`;
+const wrongColor: RGBColor = "rgb(255, 87, 51)"; 
+
+type Email = `${string}@${string}.${string}`;
+
+const email: Email = "test@gmail.com"
+
+function sendEmail(to: Email, subject: string, body: string) {
+  console.log(`Sending email to ${to} with subject "${subject}"`);
 }
 
-function renderPage(state: State, currentPage: string): string {
-  if (state.error) {
-    return `Error! Unable to load ${currentPage}: ${state.error}`;
-  } else if (state.isLoading) {
-    return `Loading ${currentPage}...`;
+type UnknownEmail = {kind: "unknown"}
+
+type User = {
+  name: string;
+  contact: Email | UnknownEmail;
+}
+
+function getEmail(user: User): Email | null {
+  if (typeof user.contact === "string") {
+    return user.contact;
   }
-  return `<h1>${currentPage}</h1>\n${state.pageText}`;
-}
-async function changePage(state: State, newPage: string) {
-  state.isLoading = true;
-  try {
-    const response = await fetch(getUrlForPage(newPage));
-    if (!response.ok) {
-      throw new Error(`Unable to load ${newPage}: ${response.statusText}`);
-    }
-    const text = await response.text();
-    state.isLoading = false;
-    state.pageText = text;
-  } catch (e) {
-    state.error = "" + e;
-  }
+  return null;
 }
 
-// right way
-interface RequestPending {
-  state: "pending";
-}
-interface RequestError {
-  state: "error";
-  error: string;
-}
-interface RequestSuccess {
-  state: "ok";
-  pageText: string;
-}
-type RequestState = RequestPending | RequestError | RequestSuccess;
-
-interface State {
-  currentPage: string;
-  requests: { [page: string]: RequestState };
+const user: User = {
+  name: "Alice",
+  contact: {kind: "unknown"}
 }
 
-function renderCustomPage(state: State) {
-  const { currentPage } = state;
-  const requestState = state.requests[currentPage];
-  switch (requestState.state) {
-    case "pending":
-      return `Loading ${currentPage}...`;
-    case "error":
-      return `Error! Unable to load ${currentPage}: ${requestState.error}`;
-    case "ok":
-      return `<h1>${currentPage}</h1>\n${requestState.pageText}`;
-  }
+interface Config {
+  retries: number;
+  verbose: boolean;
 }
+
+function makeConfig(partial: Partial<Config>): Config {
+  return { retries: 3, verbose: false, ...partial };
+}
+
+const config = makeConfig({verbose: false})
