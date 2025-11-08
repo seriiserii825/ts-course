1a2f955a4fb9b5ef9f281331f81fd36cd4dc9134
serii <seriiburduja@gmail.com>
2025-11-08 09:59:32 +0200

    feat: homework 41

--- modules/41.homework.ts
+++ modules/41.homework.ts
@@ -1,40 +1,65 @@
 export default function homework41() {
-  // Request
-  // {
-  //     animal: 'cat' | 'dog' | 'bird',
-  //     breed: string,
-  //     sterilized?: string
-  // }
+  type TAnimalType = "cat" | "dog" | "bird";
 
-  // Response #1
+  type TRequest = {
+    animal: TAnimalType;
+    breed: string;
+    sterilized?: string;
+  };
 
-  // {
-  //     status: 'available',
-  //     data: {
-  //         animal: 'cat' | 'dog' | 'bird',
-  //         breed: string,
-  //         sterilized?: string,
-  //         location: string,
-  //         age?: number
-  //     }
-  // }
+  type TResponseAvailable = {
+    status: "available";
+    data: {
+      animal: TAnimalType;
+      breed: string;
+      sterilized?: string;
+      location: string;
+      age?: number;
+    };
+  };
 
-  // Response #2
+  type TResponseNotAvailable = {
+    status: "not available";
+    data: {
+      message: string;
+      nextUpdateIn: Date;
+    };
+  };
 
-  // {
-  //     status: 'not available',
-  //     data: {
-  //         message: string,
-  //         nextUpdateIn: Date
-  //     }
-  // }
+  type TResponse = TResponseAvailable | TResponseNotAvailable;
 
-  function checkAnimalData(animal) {
-    if ("available") {
-      // Заменить условие!
+  const request: TRequest = {
+    animal: "cat",
+    breed: "siamese",
+    sterilized: "yes",
+  };
+
+  const response: TResponseAvailable = {
+    status: "available",
+    data: {
+      animal: "cat",
+      breed: "siamese",
+      sterilized: "yes",
+      location: "New York",
+      age: 2,
+    },
+  };
+
+  const responseNotAvailable: TResponseNotAvailable = {
+    status: "not available",
+    data: {
+      message: "Sorry, no animals of this breed are available at the moment.",
+      nextUpdateIn: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // через неделю
+    },
+  };
+
+  function checkAnimalData(animal: TResponse) {
+    if (animal.status === "available") {
       return animal.data;
     } else {
       return `${animal.data}, you can try in ${animal.data.nextUpdateIn}`;
     }
   }
+  
+  console.log(checkAnimalData(responseNotAvailable));
 }
