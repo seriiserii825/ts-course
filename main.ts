8c0978cf8535fc015319966f42ee0ea31d48181b
serii <seriiburduja@gmail.com>
2025-11-11 08:37:21 +0200

    feat: 49homework

--- main.ts
+++ main.ts
@@ -5,35 +5,10 @@ import cartType from "./modules/41.cartType";
 import drinksFunc from "./modules/41.drinks";
 import petRobot from "./modules/41.petRobot";
 import weatherCheck from "./modules/41.weatherCheck";
+import genericsPractice from "./modules/49.genericsPractice";
 
-interface ParentsOfUser {
-  mother: string;
-  father: string;
-}
 
-interface User<ParentsData extends ParentsOfUser> {
-  login: string;
-  age: number;
-  parents: ParentsData;
-}
-
-const user: User<{ mother: string; father: string; married: true }> = {
-  login: "ivan123",
-  age: 25,
-  parents: {
-    mother: "Anna",
-    father: "Tom",
-    married: true,
-  },
-};
-
-function depositNumber<T extends number | string>(amount: T): T {
-  return amount;
-}
-
-console.log(depositNumber(44));
-console.log(depositNumber('500'));
-
+genericsPractice();
 // weatherCheck();
 // petRobot();
 // drinksFunc();
---
+++ modules/49.genericsPractice.ts
@@ -0,0 +1,116 @@
+export default function genericsPractice() {
+  // Создать Generic-интерфейс PlayerData, который подходил бы для создания таких объектов:
+  interface PlayerData<TGame, THours, TServer> {
+    game: TGame;
+    hours: THours;
+    server: TServer;
+  }
+
+  const player1: PlayerData<string, number, string> = {
+    game: "CS:GO",
+    hours: 300,
+    server: "basic",
+  };
+
+  const player2: PlayerData<number, string, string> = {
+    game: 2048,
+    hours: "300 h.",
+    server: "arcade",
+  };
+
+  const player3: PlayerData<string, { total: number; inMenu: number }, string> = {
+    game: "Chess",
+    hours: {
+      total: 500,
+      inMenu: 50,
+    },
+    server: "chess",
+  };
+
+  // Массив данных с фигурами содержит объекты, у каждого из которых обязательно есть свойство name
+  // Каждый объект может еще содержать дополнительные свойства в случайном виде
+  // Свойство name может иметь только 4 варианта
+  // Функция calculateAmountOfFigures должна принимать массив с объектами, у которых обязательно должно быть свойство name
+  // Возвращает она объект-экземпляр AmountOfFigures
+  // Внутри себя подсчитывает сколько каких фигур было в массиве и записывает результаты в AmountOfFigures
+  // С текущими данными в консоль должно попадать:
+  // { squares: 3, circles: 2, triangles: 2, others: 1 }
+
+  interface AmountOfFigures {
+    squares: number;
+    circles: number;
+    triangles: number;
+    others: number;
+  }
+
+  function calculateAmountOfFigures(figure: IData[]): AmountOfFigures {
+    const result: AmountOfFigures = {
+      squares: 0,
+      circles: 0,
+      triangles: 0,
+      others: 0,
+    };
+
+    figure.forEach((item) => {
+      switch (item.name) {
+        case "rect":
+          result.squares += 1;
+          break;
+        case "circle":
+          result.circles += 1;
+          break;
+        case "triangle":
+          result.triangles += 1;
+          break;
+        default:
+          result.others += 1;
+          break;
+      }
+    });
+
+    return result;
+  }
+
+  type TName = "rect" | "circle" | "triangle" | "line";
+
+  interface IData {
+    name: TName;
+    data?: { [key: string]: number };
+  }
+
+  const data: IData[] = [
+    {
+      name: "rect",
+      data: { a: 5, b: 10 },
+    },
+    {
+      name: "rect",
+      data: { a: 6, b: 11 },
+    },
+    {
+      name: "triangle",
+      data: { a: 5, b: 10, c: 14 },
+    },
+    {
+      name: "line",
+      data: { l: 15 },
+    },
+    {
+      name: "circle",
+      data: { r: 10 },
+    },
+    {
+      name: "circle",
+      data: { r: 5 },
+    },
+    {
+      name: "rect",
+      data: { a: 15, b: 7 },
+    },
+    {
+      name: "triangle",
+    },
+  ];
+
+  console.log(calculateAmountOfFigures(data));
+}
